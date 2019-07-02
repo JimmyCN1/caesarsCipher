963ac66d5266af2956455aa72ef375ec0e079463
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-02 14:42:14 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Caesar's Cipher
+
+### This will repository will contain an algortihm that implements "caesar's cipher" decription algorithm
+
+#### Credits: Jimmy Njoku
---
+++ caesarsCipher.js
@@ -0,0 +1,10 @@
+function rot13(str) {
+  return str;
+}
+
+// Tests
+
+rot13("SERR PBQR PNZC"); // should decode to FREE CODE CAMP
+rot13("SERR CVMMN!"); // should decode to FREE PIZZA!
+rot13("SERR YBIR?"); // should decode to FREE LOVE?
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."); // should decode to THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
